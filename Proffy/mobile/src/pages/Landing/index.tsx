35da9aaa59e69fd638ae878f7d68f957c5ed1776
Alexandre Santos <ale.santos49@gmail.com>
2020-08-11 20:41:02 -0300

    Finalização do desenvolvimento mobile da aplicação

--- Proffy/mobile/src/pages/Landing/index.tsx
+++ Proffy/mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Image,Text,TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image,Text } from 'react-native';
 import styles from './styles';
 import landingImg from '../../assets/images/landing.png';
 import sutdyIcon from '../../assets/images/icons/study.png';
@@ -7,9 +7,18 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 import {useNavigation} from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
 
 function Landing(){
     const {navigate} = useNavigation();
+    const [ totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(()=>{
+        api.get('connections').then(response =>{
+            const {total}= response.data;
+            setTotalConnections(total);
+        })
+    },[]);
 
     function handleNavigateToGiveClassesPages(){
         navigate('GiveClasses');
@@ -47,7 +56,7 @@ function Landing(){
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 285 conexões já realizadas {' '}
+                Total de {totalConnections} conexões já realizadas {' '}
                 <Image source={heartIcon}/>
             </Text>
         </View>
